820724974ec307115912e7d3bee5e33a591f7505
AJ Bucci <ajbucci@gmail.com>
2023-12-30 18:13:04 -0500

    fix(flatpak): force x11

--- flatpak-build.sh
+++ flatpak-build.sh
@@ -1,6 +1,6 @@
-poetry build
-python flatpak-poetry-generator --production poetry.lock
+#poetry build
+#python flatpak-poetry-generator.py --production poetry.lock
 flatpak-builder --force-clean --repo=flatpak-streamdeck build/ io.github.streamdeck-linux-gui.yaml --verbose
-flatpak remote-add --user flatpak-streamdeck flatpak-streamdeck --no-gpg-verify
+#flatpak remote-add --user flatpak-streamdeck flatpak-streamdeck --no-gpg-verify
 flatpak uninstall io.github.streamdeck-linux-gui
 flatpak --user install flatpak-streamdeck io.github.streamdeck-linux-gui
